bb620e31b501afed0347e91b126f9f01c9073b05
mertwole <mertwole@gmail.com>
2024-07-12 01:16:32 +0800

    Remove cached API concept

--- src/api/ledger.rs
+++ src/api/ledger.rs
@@ -1,31 +1,13 @@
-use super::{Api, ApiCache};
+pub trait LedgerApiT {}
 
-pub trait LedgerApiT: Api {}
+pub struct LedgerApi {}
 
-pub struct LedgerApiCache {}
-
-impl<A: LedgerApiT> ApiCache<A> for LedgerApiCache {
-    async fn init(api: &A) -> Self {
-        LedgerApiCache {}
-    }
-}
-
-mod api {
-    use super::*;
-
-    pub struct LedgerApi {}
-
-    impl Api for LedgerApi {}
-
-    impl LedgerApiT for LedgerApi {}
-}
+impl LedgerApiT for LedgerApi {}
 
 mod mock {
     use super::*;
 
     pub struct LedgerApiMock {}
-
-    impl Api for LedgerApiMock {}
 
     impl LedgerApiT for LedgerApiMock {}
 }
