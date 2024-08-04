3cda08764cbbf82777125eaba251dcee43323844
mertwole <mertwole@gmail.com>
2024-08-04 14:35:45 +0700

    Complete list of API methods

--- src/api/blockchain_monitoring.rs
+++ src/api/blockchain_monitoring.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use rust_decimal::Decimal;
 
 use super::common::{Account, Network};
@@ -7,7 +9,29 @@ impl_cache_for_api! {
     pub trait BlockchainMonitoringApiT {
         // TODO: Pass `Account` as a reference.
         async fn get_balance(&self, network: Network, account: Account) -> Decimal;
+
+        async fn get_transactions(&self, network: Network, account: Account) -> Vec<TransactionUid>;
+
+        // TODO: Pass `TransactionUid` as a reference.
+        async fn get_transaction_info(&self, network: Network, tx_uid: TransactionUid) -> TransactionInfo;
     }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct TransactionUid {
+    uid: String,
+}
+
+#[derive(Clone)]
+pub struct TransactionInfo {
+    ty: TransactionType,
+    timestamp: Instant,
+}
+
+#[derive(Clone)]
+pub enum TransactionType {
+    Deposit { from: Account, amount: Decimal },
+    Withdraw { to: Account, amount: Decimal },
 }
 
 pub struct BlockchainMonitoringApi {}
@@ -20,6 +44,18 @@ impl BlockchainMonitoringApi {
 
 impl BlockchainMonitoringApiT for BlockchainMonitoringApi {
     async fn get_balance(&self, _network: Network, _account: Account) -> Decimal {
+        todo!()
+    }
+
+    async fn get_transactions(&self, _network: Network, _account: Account) -> Vec<TransactionUid> {
+        todo!()
+    }
+
+    async fn get_transaction_info(
+        &self,
+        _network: Network,
+        _tx_uid: TransactionUid,
+    ) -> TransactionInfo {
         todo!()
     }
 }
@@ -37,6 +73,22 @@ pub mod mock {
 
     impl BlockchainMonitoringApiT for BlockchainMonitoringApiMock {
         async fn get_balance(&self, _network: Network, _account: Account) -> Decimal {
+            todo!()
+        }
+
+        async fn get_transactions(
+            &self,
+            _network: Network,
+            _account: Account,
+        ) -> Vec<TransactionUid> {
+            todo!()
+        }
+
+        async fn get_transaction_info(
+            &self,
+            _network: Network,
+            _tx_uid: TransactionUid,
+        ) -> TransactionInfo {
             todo!()
         }
     }
