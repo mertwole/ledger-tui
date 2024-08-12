7ea2b05cd0033ab419c917ab502c0bbcec82f627
mertwole <mertwole@gmail.com>
2024-08-12 15:37:24 +0700

    Fetch price history from API

--- app/src/api/coin_price.rs
+++ app/src/api/coin_price.rs
@@ -1,7 +1,11 @@
 use std::time::Instant;
 
 use api_proc_macro::implement_cache;
-use binance_spot_connector_rust::{market, ureq::BinanceHttpClient};
+use binance_spot_connector_rust::{
+    market::{self, klines::KlineInterval},
+    ureq::BinanceHttpClient,
+};
+use chrono::{DateTime, Utc};
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use serde::Deserialize;
@@ -47,13 +51,64 @@ pub struct CoinPriceApi {
     client: BinanceHttpClient,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-#[allow(dead_code)]
 struct BinanceApiMarketAvgPriceResponse {
     mins: u32,
     price: Decimal,
     close_time: u64,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(from = "BinanceApiKlineSerde")]
+struct BinanceApiKline {
+    open_time: DateTime<Utc>,
+    open_price: Decimal,
+    high_price: Decimal,
+    low_price: Decimal,
+    close_price: Decimal,
+    volume: Decimal,
+    close_time: DateTime<Utc>,
+    quote_asset_volume: Decimal,
+    number_of_trades: u32,
+    taker_buy_base_asset_volume: Decimal,
+    taker_buy_quote_asset_volume: Decimal,
+    unused_field: String,
+}
+
+#[derive(Deserialize)]
+struct BinanceApiKlineSerde(
+    #[serde(with = "chrono::serde::ts_milliseconds")] DateTime<Utc>,
+    Decimal,
+    Decimal,
+    Decimal,
+    Decimal,
+    Decimal,
+    #[serde(with = "chrono::serde::ts_milliseconds")] DateTime<Utc>,
+    Decimal,
+    u32,
+    Decimal,
+    Decimal,
+    String,
+);
+
+impl From<BinanceApiKlineSerde> for BinanceApiKline {
+    fn from(value: BinanceApiKlineSerde) -> Self {
+        BinanceApiKline {
+            open_time: value.0,
+            open_price: value.1,
+            high_price: value.2,
+            low_price: value.3,
+            close_price: value.4,
+            volume: value.5,
+            close_time: value.6,
+            quote_asset_volume: value.7,
+            number_of_trades: value.8,
+            taker_buy_base_asset_volume: value.9,
+            taker_buy_quote_asset_volume: value.10,
+            unused_field: value.11,
+        }
+    }
 }
 
 impl CoinPriceApi {
@@ -80,10 +135,20 @@ impl CoinPriceApiT for CoinPriceApi {
 
     async fn get_price_history(
         &self,
-        _from: Coin,
-        _to: Coin,
+        from: Coin,
+        to: Coin,
         _interval: TimePeriod,
     ) -> Option<PriceHistory> {
+        let pair = [from.to_api_string(), to.to_api_string()].concat();
+        let history = self
+            .client
+            .send(market::klines(&pair, KlineInterval::Minutes1))
+            .unwrap()
+            .into_body_str()
+            .unwrap();
+
+        let _history: Vec<BinanceApiKline> = serde_json::from_str(&history).unwrap();
+
         todo!()
     }
 }
