f61ae2a6302d2ef249cede6a64893d911852defc
mertwole <mertwole@gmail.com>
2024-08-22 00:09:49 +0700

    Add format_address

--- app/src/screen/common.rs
+++ app/src/screen/common.rs
@@ -24,3 +24,46 @@ pub fn render_centered_text(frame: &mut Frame, area: Rect, text: Text) {
 
     frame.render_widget(text, area);
 }
+
+pub fn format_address(address: &str, max_symbols: usize) -> String {
+    if max_symbols <= 3 {
+        return "".to_string();
+    }
+
+    if max_symbols <= 8 {
+        return "...".to_string();
+    }
+
+    let part_size = (max_symbols - 3) / 2;
+    let part_size = part_size.min(8);
+
+    if address.len() <= part_size * 2 {
+        return address.to_string();
+    }
+
+    format!(
+        "{}...{}",
+        &address[..part_size],
+        &address[(address.len() - part_size)..]
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter;
+
+    use itertools::Itertools;
+
+    use super::format_address;
+
+    #[test]
+    fn test_format_address() {
+        let address_lengths = [0, 2, 8, 10, 100].into_iter();
+        let max_lengths = [0, 3, 5, 6, 8, 10, 100].into_iter();
+
+        for (addr_len, max_len) in address_lengths.cartesian_product(max_lengths) {
+            let address: String = iter::repeat('0').take(addr_len).collect();
+            assert!(format_address(&address, max_len).len() <= max_len);
+        }
+    }
+}
