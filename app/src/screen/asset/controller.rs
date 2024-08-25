22ae5bfaca357c7e2d149ca468cae6e32dfd03ec
mertwole <mertwole@gmail.com>
2024-08-26 01:42:48 +0700

    Move asset screen to a new macro

--- app/src/screen/asset/controller.rs
+++ app/src/screen/asset/controller.rs
@@ -1,12 +1,13 @@
+use input_mapping_common::InputMappingT;
 use input_mapping_derive::InputMapping;
-use ratatui::crossterm::event::{Event, KeyCode};
+use ratatui::crossterm::event::Event;
 
 use crate::{
     api::{
         blockchain_monitoring::BlockchainMonitoringApiT, coin_price::CoinPriceApiT,
         ledger::LedgerApiT,
     },
-    screen::{EventExt, OutgoingMessage, ScreenName},
+    screen::{OutgoingMessage, ScreenName},
 };
 
 use super::{Model, TimePeriod};
@@ -55,43 +56,22 @@ pub(super) fn process_input<L: LedgerApiT, C: CoinPriceApiT, M: BlockchainMonito
     event: &Event,
     model: &mut Model<L, C, M>,
 ) -> Option<OutgoingMessage> {
-    if event.is_key_pressed(KeyCode::Char('q')) {
-        return Some(OutgoingMessage::Exit);
+    let event = InputEvent::map_event(event.clone())?;
+
+    match event {
+        InputEvent::Quit => Some(OutgoingMessage::Exit),
+        InputEvent::Back => Some(OutgoingMessage::Back),
+        InputEvent::OpenDepositScreen => Some(OutgoingMessage::SwitchScreen(ScreenName::Deposit)),
+        InputEvent::SelectTimeInterval(event) => {
+            model.selected_time_period = match event {
+                SelectTimeInterval::Day => TimePeriod::Day,
+                SelectTimeInterval::Week => TimePeriod::Week,
+                SelectTimeInterval::Month => TimePeriod::Month,
+                SelectTimeInterval::Year => TimePeriod::Year,
+                SelectTimeInterval::All => TimePeriod::All,
+            };
+
+            None
+        }
     }
-
-    if event.is_key_pressed(KeyCode::Char('b')) {
-        return Some(OutgoingMessage::Back);
-    }
-
-    if event.is_key_pressed(KeyCode::Char('s')) {
-        return Some(OutgoingMessage::SwitchScreen(ScreenName::Deposit));
-    }
-
-    process_time_interval_selection(event, model);
-
-    None
-}
-
-fn process_time_interval_selection<L: LedgerApiT, C: CoinPriceApiT, M: BlockchainMonitoringApiT>(
-    event: &Event,
-    model: &mut Model<L, C, M>,
-) {
-    match () {
-        () if event.is_key_pressed(KeyCode::Char('d')) => {
-            model.selected_time_period = TimePeriod::Day;
-        }
-        () if event.is_key_pressed(KeyCode::Char('w')) => {
-            model.selected_time_period = TimePeriod::Week;
-        }
-        () if event.is_key_pressed(KeyCode::Char('m')) => {
-            model.selected_time_period = TimePeriod::Month;
-        }
-        () if event.is_key_pressed(KeyCode::Char('y')) => {
-            model.selected_time_period = TimePeriod::Year;
-        }
-        () if event.is_key_pressed(KeyCode::Char('a')) => {
-            model.selected_time_period = TimePeriod::All;
-        }
-        _ => {}
-    };
 }
