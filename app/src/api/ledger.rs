ed8d78351e6f119e8ab90ef98e57c0187756d5de
mertwole <mertwole@gmail.com>
2024-10-29 12:34:25 +0400

    Process error

--- app/src/api/ledger.rs
+++ app/src/api/ledger.rs
@@ -2,7 +2,7 @@ use std::{hash::Hash, str::FromStr};
 
 use api_proc_macro::implement_cache;
 use async_trait::async_trait;
-use ledger_apdu::APDUCommand;
+use ledger_apdu::{APDUCommand, APDUErrorCode};
 use ledger_transport_hid::{
     hidapi::{DeviceInfo as LedgerDeviceInfo, HidApi},
     TransportNativeHID,
@@ -92,11 +92,12 @@ impl LedgerApiT for LedgerApi {
 }
 
 impl LedgerApi {
+    // TODO: It's just a showcase of communicating with bitcoin app.
     async fn discover_bitcoin_accounts(&self, device: &Device) -> Vec<Account> {
         let transport = TransportNativeHID::open_device(&self.hid_api, &device.info).unwrap();
 
         let data = &[
-            &[1u8][..],                                // Display
+            &[0u8][..],                                // Display
             &[5u8][..], // Number of BIP 32 derivations to perform (max 8)
             &((1u32 << 31) ^ 84u32).to_be_bytes()[..], // 1st derivation index (big endian)
             &((1u32 << 31) ^ 0u32).to_be_bytes()[..], // 2nd derivation index (big endian)
@@ -115,13 +116,22 @@ impl LedgerApi {
         };
 
         let response = transport.exchange(&command).unwrap();
-        let response = String::from_utf8(response.data().to_vec()).unwrap();
-        let _xpub = bitcoin::bip32::ExtendedPubKey::from_str(&response).unwrap();
 
-        vec![]
+        match response.error_code() {
+            Err(_) => return vec![],
+            Ok(APDUErrorCode::NoError) => {}
+            Ok(_) => return vec![],
+        }
+
+        let response = String::from_utf8(response.data().to_vec()).unwrap();
+        let xpub = bitcoin::bip32::ExtendedPubKey::from_str(&response).unwrap();
+
+        let public_key = xpub.public_key.to_string();
+
+        vec![Account { public_key }]
     }
 
-    async fn discover_ethereum_accounts(&self, device: &Device) -> Vec<Account> {
+    async fn discover_ethereum_accounts(&self, _device: &Device) -> Vec<Account> {
         vec![]
     }
 }
