05350e7d1fc6303f556148ced6de53f315939477
mertwole <mertwole@gmail.com>
2025-03-02 01:08:45 +0400

    Implement

--- app/src/api/storage.rs
+++ app/src/api/storage.rs
@@ -1,30 +1,85 @@
+use std::{collections::HashMap, path::PathBuf};
+
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait StorageApiT: Send + Sync + 'static {}
+pub trait StorageApiT: Send + Sync + 'static {
+    async fn save(&mut self, name: &str, content: String);
 
-pub struct StorageApi {}
+    async fn load(&mut self, name: &str) -> Option<String>;
+}
+
+pub struct StorageApi {
+    path: PathBuf,
+    cache: HashMap<String, String>,
+}
 
 impl StorageApi {
-    pub async fn new() -> Self {
-        Self {}
+    pub fn new(path: PathBuf) -> Self {
+        if !path.is_dir() {
+            panic!("Expected directory");
+        }
+
+        Self {
+            path,
+            cache: HashMap::new(),
+        }
     }
 }
 
 #[async_trait]
-impl StorageApiT for StorageApi {}
+impl StorageApiT for StorageApi {
+    async fn save(&mut self, name: &str, content: String) {
+        tokio::fs::write(self.path.join(name), content.clone())
+            .await
+            .unwrap();
+
+        self.cache.insert(name.to_string(), content);
+    }
+
+    async fn load(&mut self, name: &str) -> Option<String> {
+        if let Some(cached) = self.cache.get(name) {
+            return Some(cached.clone());
+        }
+
+        let path = self.path.join(name);
+
+        let file_exists = tokio::fs::try_exists(&path).await;
+        if !matches!(file_exists, Ok(true)) {
+            return None;
+        }
+
+        let content = tokio::fs::read_to_string(path).await.unwrap();
+
+        self.cache.insert(name.to_string(), content.clone());
+
+        Some(content)
+    }
+}
 
 pub mod mock {
     use super::*;
 
-    pub struct StorageApiMock {}
+    pub struct StorageApiMock {
+        storage: HashMap<String, String>,
+    }
 
     impl StorageApiMock {
         pub fn new() -> Self {
-            Self {}
+            Self {
+                storage: HashMap::new(),
+            }
         }
     }
 
     #[async_trait]
-    impl StorageApiT for StorageApiMock {}
+    impl StorageApiT for StorageApiMock {
+        async fn save(&mut self, name: &str, content: String) {
+            self.storage.insert(name.to_string(), content);
+        }
+
+        async fn load(&mut self, name: &str) -> Option<String> {
+            self.storage.get(name).cloned()
+        }
+    }
 }
